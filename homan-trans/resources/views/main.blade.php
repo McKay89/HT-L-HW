b038e9248dcd51b82456a88919f5d0b8632159e7
McKay <kg89szeged@gmail.com>
2024-04-03 18:53:38 +0200

    [ UPDATE ] filter by name & sorting added

--- homan-trans/resources/views/main.blade.php
+++ homan-trans/resources/views/main.blade.php
@@ -5,14 +5,27 @@
 @unless(count($episodes) == 0)
     <div class="w-full flex justify-center mt-9">
         <table class="w-full border-separate border-spacing-2 border border-slate-400">
-            <thead class="bg-nav text-white h-12 text-xl">
+            <thead class="h-12 text-xl">
                 <tr>
-                    <th>ID <i class="fa-solid fa-sort"></i></th>
-                    <th>Name <i class="fa-solid fa-sort"></i></th>
-                    <th>Episode <i class="fa-solid fa-sort"></i></th>
-                    <th>URL <i class="fa-solid fa-sort"></i></th>
-                    <th>Air Date <i class="fa-solid fa-sort"></i></th>
-                    <th>Created <i class="fa-solid fa-sort"></i></th>
+                    <th></th>
+                    <th>
+                        <input type="text" id="filterByName" class="p-1 text-center" placeholder="Filter by 'Name'">
+                    </th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th>
+                        <input type="text" id="filterByCreatedFrom" class="w-36 p-1 text-center" placeholder="FROM"> -
+                        <input type="text" id="filterByCreatedTo" class="w-36 p-1 text-center" placeholder="TO">
+                    </th>
+                </tr>
+                <tr class="bg-nav text-white">
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_id', 'direction' => 'asc']) }}">ID <i class="fa-solid fa-sort"></i></a></th>
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_name', 'direction' => 'asc']) }}">Name <i class="fa-solid fa-sort"></i></a></th>
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_episode', 'direction' => 'asc']) }}">Episode <i class="fa-solid fa-sort"></i></a></th>
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_url', 'direction' => 'asc']) }}">URL <i class="fa-solid fa-sort"></i></a></th>
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_air_date', 'direction' => 'asc']) }}">Air Date <i class="fa-solid fa-sort"></i></a></th>
+                    <th><a href="{{ route('sorted-episodes', ['column' => 'ep_created', 'direction' => 'asc']) }}">Created <i class="fa-solid fa-sort"></i></a></th>
                 </tr>
             </thead>
             <tbody>
