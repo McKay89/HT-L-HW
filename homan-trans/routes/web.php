d844d9779750d746159bc3096de0e2b15d4ccf0b
McKay <kg89szeged@gmail.com>
2024-04-03 17:56:50 +0200

    [ UPDATE ] root route and fetch-api route created

--- homan-trans/routes/web.php
+++ homan-trans/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Episode;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main', [
+        'heading' => 'Rick & Morty Episodes',
+        'episodes' => Episode::paginate(7)
+    ]);
 });
+
+Route::get('/fetch-api', [ApiController::class,'index'])->name('index');
