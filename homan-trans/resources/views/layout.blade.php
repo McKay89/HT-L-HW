20c6b1a7c17e49d1d58fa21bb4e64ec411554ac6
McKay <kg89szeged@gmail.com>
2024-04-03 20:47:48 +0200

    [ UPDATE ] loading body added

--- homan-trans/resources/views/layout.blade.php
+++ homan-trans/resources/views/layout.blade.php
@@ -20,7 +20,7 @@
                     'main': '#9898A9',
                     'white': '#ffffff',
                     'black': '#000000',
-                    'loader': '#00000077',
+                    'loader': '#000000aa',
                     'button': '#6666DF',
                     'button-hover': '#5050C9'
                 }
@@ -44,7 +44,11 @@
     </main>
 
     <div id="loader" class="absolute top-0 left-0 w-full h-full bg-loader text-white" style="display: none;">
-        Töltés...
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <img src="images/loader.gif" width="600" height="600"><br>
+            <span class="flex justify-center text-3xl">Loading...</span><br>
+            <span class="flex justify-center text-2xl">This will take&nbsp; <b>~2 minutes</b> !</span>
+        </div>
     </div>
 
     <script>
