7c9b4bc3de479fc9fab5ea8fd52b539b5e620dd5
McKay <kg89szeged@gmail.com>
2024-04-03 18:54:45 +0200

    [ UPDATE ] 'sorted-episodes' route added

--- homan-trans/routes/web.php
+++ homan-trans/routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Episode;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
+// Root route
 Route::get('/', function () {
     return view('main', [
         'heading' => 'Rick & Morty Episodes',
@@ -11,4 +12,8 @@ Route::get('/', function () {
     ]);
 });
 
+// Route for fetching data and upload to Database
 Route::get('/fetch-api', [ApiController::class,'index'])->name('index');
+
+// Route for sorting data
+Route::get('/sorted-episodes/{column}/{direction}', [ApiController::class, 'sortedEpisodes'])->name('sorted-episodes');
