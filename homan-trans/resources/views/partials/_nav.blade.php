635d45557f0dbdbb3da1af016a23f54e1feb9105
McKay <kg89szeged@gmail.com>
2024-04-04 09:46:05 +0200

    [ ADD ] nav partial created

---
+++ homan-trans/resources/views/partials/_nav.blade.php
@@ -0,0 +1,8 @@
+<nav class="h-24 w-full bg-nav text-white flex">
+    <div class="w-1/2 text-3xl font-semibold flex justify-start ml-10 my-6">
+        <h1><a href="{{ url('/') }}">Homan-Trans - Laravel Homework</a></h1>
+    </div>
+    <div class="w-1/2  flex justify-end mr-10 my-6">
+        <button id="fetchDataButton" class="bg-button hover:bg-button-hover transition text-white font-bold py-2 px-4 border border-blue-700 rounded">Download Data</button>
+    </div>
+</nav>
