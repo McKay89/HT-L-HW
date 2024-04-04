ba391e5222e821059ce3ace4e5a53463984c1e82
McKay <kg89szeged@gmail.com>
2024-04-04 09:45:58 +0200

    [ ADD ] loader partial created

---
+++ homan-trans/resources/views/partials/_loader.blade.php
@@ -0,0 +1,7 @@
+<div id="loader" class="absolute top-0 left-0 w-full h-full bg-loader text-white" style="display: none;">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <img src="images/loader.gif" width="600" height="600"><br>
+        <span class="flex justify-center text-3xl">Loading...</span><br>
+        <span class="flex justify-center text-2xl">This will take&nbsp; <b>~2 minutes</b> !</span>
+    </div>
+</div>
